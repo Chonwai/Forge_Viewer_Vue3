9f78e154dae142e0644c1d7d25090a08a9e92a14
Chonwai <chonwaiun@gmail.com>
2021-01-16 14:55:51 +0800

    Add: Added the axios.

--- src/components/Forge.vue
+++ src/components/Forge.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="relative">
+        <div class="p-2">
+            <input class="m-2 p-1 border" type="text" placeholder="Width" />
+            <input class="m-2 p-1 border" type="text" placeholder="Height" />
+            <button class="m-2 p-1 border" type="submit">Update!</button>
+        </div>
         <div class="w-full h-full" id="forge-viewer"></div>
     </div>
 </template>
@@ -13,16 +18,18 @@ export default {
     created() {
         this.init();
     },
-    mounted() {},
     methods: {
         init() {
+            this.showForge();
+        },
+        showForge() {
             var viewer;
             var options = {
                 env: 'AutodeskProduction',
                 api: 'derivativeV2', // for models uploaded to EMEA change this option to 'derivativeV2_EU'
                 getAccessToken: async function (onTokenReady) {
                     var token =
-                        'eyJhbGciOiJIUzI1NiIsImtpZCI6Imp3dF9zeW1tZXRyaWNfa2V5In0.eyJzY29wZSI6WyJjb2RlOmFsbCIsImRhdGE6d3JpdGUiLCJkYXRhOnJlYWQiLCJidWNrZXQ6Y3JlYXRlIiwiYnVja2V0OmRlbGV0ZSIsImJ1Y2tldDpyZWFkIl0sImNsaWVudF9pZCI6IjNIRzBwQWNmTXBCd0hRMjB0M29HN0RpbnhQTDB3Nm1HIiwiYXVkIjoiaHR0cHM6Ly9hdXRvZGVzay5jb20vYXVkL2p3dGV4cDYwIiwianRpIjoiMHdFMlhEZ0E2NXowck16bE55a3NYR2hWVzBCcVQzWHloUU5lTExzeGlLa296SXA3MGxaMFcyZWwwNERoR2hteiIsImV4cCI6MTYxMDUzMTk4N30.btOMBNkuXU03qU1yFW_8Qf6XkupPDjED0OZNZ-av1zM';
+                        'eyJhbGciOiJIUzI1NiIsImtpZCI6Imp3dF9zeW1tZXRyaWNfa2V5In0.eyJzY29wZSI6WyJjb2RlOmFsbCIsImRhdGE6d3JpdGUiLCJkYXRhOnJlYWQiLCJidWNrZXQ6Y3JlYXRlIiwiYnVja2V0OmRlbGV0ZSIsImJ1Y2tldDpyZWFkIl0sImNsaWVudF9pZCI6IjNIRzBwQWNmTXBCd0hRMjB0M29HN0RpbnhQTDB3Nm1HIiwiYXVkIjoiaHR0cHM6Ly9hdXRvZGVzay5jb20vYXVkL2p3dGV4cDYwIiwianRpIjoid0dpOU5iN2hqazdtY1BsV3llUXhmSXZ0Um42Z0tpMkZtbVNRZFc1WmZ1QXZMZzdrNDBmQUM0TE5nWWdrWTE3byIsImV4cCI6MTYxMDYxODA3MH0.4eQpGMub0b_YgdGXYB9zM6T5kP0Fh0AHx5VQ026gKro';
                     var timeInSeconds = 3600; // Use value provided by Forge Authentication (OAuth) API
                     await onTokenReady(token, timeInSeconds);
                 },
@@ -32,7 +39,7 @@ export default {
                 var htmlDiv = document.getElementById('forge-viewer');
                 viewer = new window.Autodesk.Viewing.GuiViewer3D(htmlDiv);
                 var documentId =
-                    'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6ZWRpc29uX2J1Y2tldC9uZXdfYm94LmlwdA';
+                    'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6ZWRpc29uX2J1Y2tldC91cGRhdGVfYm94LmlwdA';
                 window.Autodesk.Viewing.Document.load(
                     documentId,
                     viewerDocument => {
